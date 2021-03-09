906fe6def6e0b57cfc0bf284a1865842dadd1a17
John Cass <John@Johns-MacBook-Pro.local>
2021-03-09 17:33:00 -0600

    reset values

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,16 +2,26 @@
 import os 
 import csv
 
+#lists to store data
+
+
+#reset values at 0
+total_months = 0
+total_revenue = 0
+avg_revenue_change = 0
+greatest_increase = 0
+greatest_decrease = 0
+
 #set path for the csv file
 pybank_data = os.path.join("..", "python-challenge", "PyBank", "Resources", "pybank_data.csv")
 
 with open(pybank_data) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
-    #print header 
+    #read header row first and print header 
     csv_header = next(csv_file)
     print(f"Header: {csv_header}")
 
 
-      
 
+        
